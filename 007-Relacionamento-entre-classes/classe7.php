84d86b8be8c6ed70dfc384556e1a6204f25a2772
Richard Oliveira <richard.097@hotmail.com>
2022-09-28 16:44:53 -0300

    007.1

---
+++ 007-Relacionamento-entre-classes/classe7.php
@@ -0,0 +1,115 @@
+<?php
+class Lutador
+{
+    //Atributos
+    private $nome;
+    private $nacionalidade;
+    private $idade;
+    private $altura;
+    private $peso;
+    private $categoria;
+    private $vitorias;
+    private $derrotas;
+    private $empates;
+    //Métodos constrtutores
+    public function __construct($no, $na, $id, $al, $pe, $vi, $de, $em)
+    {
+        $this->nome = $no;
+        $this->nacionalidade = $na;
+        $this->idade = $id;
+        $this->altura = $al;
+        $this->peso = $pe;
+        $this->vitorias = $vi;
+        $this->derrotas = $de;
+        $this->empates = $em;
+    }
+    //Métodos
+    public function apresentar()
+    {
+    }
+    public function status()
+    {
+    }
+    public function ganharLuta()
+    {
+    }
+    public function perderLuta()
+    {
+    }
+    public function empatarLuta()
+    {
+    }
+    //Getters e Setters
+    public function getNome()
+    {
+        return $this->nome;
+    }
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+    public function getNacionalidade()
+    {
+        return $this->nacionalidade;
+    }
+    public function setNacionalidade($nacionalidade)
+    {
+        $this->nacionalidade = $nacionalidade;
+    }
+    function getIdade()
+    {
+        return $this->idade;
+    }
+    public function setIdade($idade)
+    {
+        $this->idade = $idade;
+    }
+    public function getAltura()
+    {
+        return $this->altura;
+    }
+    public function setAltura($altura)
+    {
+        $this->altura = $altura;
+    }
+    public function getPeso()
+    {
+        return $this->peso;
+    }
+    public function setPeso($peso)
+    {
+        $this->peso = $peso;
+    }
+    public function getCategoria()
+    {
+        return $this->categoria;
+    }
+    public function setCategoria($categoria)
+    {
+        $this->categoria = $categoria;
+    }
+    public function getVitorias()
+    {
+        return $this->vitorias;
+    }
+    public function setVitorias($vitorias)
+    {
+        $this->vitorias = $vitorias;
+    }
+    public function getDerrotas()
+    {
+        return $this->derrotas;
+    }
+    public function setDerrotas($derrotas)
+    {
+        $this->derrotas = $derrotas;
+    }
+    public function getEmpates()
+    {
+        return $this->empates;
+    }
+    public function setEmpates($empates)
+    {
+        $this->empates = $empates;
+    }
+}
